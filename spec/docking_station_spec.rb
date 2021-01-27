12bb1bded4f498cbef5b1fc6276f05988b9b822f
Maiara <maiarinhah.eu@gmail.com>
2021-01-27 20:48:39 +0000

    Added instance variable for bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,5 +7,19 @@ describe DockingStation do
         bike = subject.release_bike
         expect(bike).to be_working
     end 
+
+    it 'docks a bike' do
+        bike = Bike.new
+        expect(subject.dock(bike)).to eq bike
+    end 
+
+    it 'allows to return a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+
+        expect(subject.bike).to eq bike
+    end 
+
+    it { is_expected.to respond_to(:bike) }
 end 
 
